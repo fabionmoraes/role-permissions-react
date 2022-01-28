ef833b43dab2a5f9eba9cf3c09a07e84c46742cd
Fabio Moraes <fabio.moraes@gsd.com.br>
2022-01-28 16:11:59 -0300

    Atualizaçao nextPermission

---
+++ src/type.ts
@@ -0,0 +1,16 @@
+export interface IHandlePermissions {
+  permissions: any[];
+  event: any;
+}
+
+export interface IDataPermissions {
+  index: number;
+  item: any;
+  permissions: any;
+}
+
+export interface IPermissionsReturnProps {
+  value: string;
+  name: string;
+  checked: boolean;
+}
